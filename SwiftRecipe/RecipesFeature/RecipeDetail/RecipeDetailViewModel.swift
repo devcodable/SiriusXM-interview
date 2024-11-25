a436eacb54033e933221b04f5087c7fd97dca317
David <dacugil97@gmail.com>
2024-11-25 13:17:00 +0000

    REC-004 - Implemented RecipeDetailViewModel

--- SwiftRecipe/RecipesFeature/RecipeDetail/RecipeDetailViewModel.swift
+++ SwiftRecipe/RecipesFeature/RecipeDetail/RecipeDetailViewModel.swift
@@ -4,13 +4,44 @@ import Foundation
 //TODO: - #4 Implement Details ViewModel using RecipeService
 
 @MainActor
-final class RecipeDetailViewModel {
+final class RecipeDetailViewModel: ObservableObject {
     let recipe: Recipe
     private let service = RecipeService()
+    
+    @Published var state: State
 
     init(recipe: Recipe) {
         self.recipe = recipe
+        self.state = .loading
     }
     
-    func fetchRecipeDetails() async {}
+    func fetchRecipeDetails() async {
+        do {
+            let result = try await service.fetchRecipeDetails(id: recipe.id)
+            state = .success(result)
+        } catch {
+            print("Error while fetching query for recipe with id: \(recipe.id). \n Error description: \(error.localizedDescription)")
+            state = .failure("Oops, something went wrong.")
+        }
+    }
+    
+    func getRatingStarImage(_ starNumber: Int) -> String {
+        let fullStar = "star.fill"
+        let halfStar = "star.leadinghalf.filled"
+        let emptyStar = "star"
+        
+        if starNumber * 20 <= Int(recipe.socialRank) {
+            return fullStar
+        } else if starNumber * 20 <= Int(recipe.socialRank) + 10 {
+            return halfStar
+        } else {
+            return emptyStar
+        }
+    }
+    
+    enum State: Equatable {
+        case loading
+        case success(Recipe)
+        case failure(String)
+    }
 }
